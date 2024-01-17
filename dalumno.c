d0dc9eb9799d2aedb399e49fbd5873e810d9a40a
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2024-01-17 15:32:05 -0500

    Estructura base de ingreso y imprimir los datos que uno pide

--- dalumno.c
+++ dalumno.c
@@ -11,6 +11,33 @@ int main(int argc, char const *argv[])
         char carrera[50];
         float promedio;
     };
+    /*Se pide al usuario que ingrese el numero de alumnos que va a ingresar*/
+    int numerodealumnos;
+    printf("Ingrese el numero de estudiantes que va a ingresar");
+    scanf("%d", &numerodealumnos);
+    /* Declarar un arreglo de estructuras para almacenar los datos de los alumnos */
+    struct dalumno alumnos[numerodealumnos];
+    /*Leer lo que uno va ir ingresando*/
+    for (int i = 0; i < numerodealumnos; i++)
+    {
+        printf("\n Ingresar datos del alumno %d:\n", i + 1);
+        printf("Nombre del Alumno : ");
+        scanf("%s", alumnos[i].nombre);
+        printf("Direccion del alumno : ");
+        scanf("%s", alumnos[i].direccion);
+        printf("Carrera del Alumno: ");
+        scanf("%s", alumnos[i].carrera);
+    }
+    /*Imprimir todo los datos que se obtuvo*/
+    printf("\n Alumnos \n");
+    printf("\n");
+    for (int i = 0; i < numerodealumnos; i++)
+    {
+        printf("Nombre %s:\n", alumnos[i].nombre);
+        printf("Direccion %s:\n", alumnos[i].direccion);
+        printf("Carrera %s:\n", alumnos[i].carrera);
+        printf("\n");
+    }
 
     return 0;
 }
