6c610dc1872cc10651aa4d3e1669bbfefe9a4938
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2024-01-17 15:10:22 -0500

    Inicio del codigo

---
+++ dalumno.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /*Estructura del uso de estructuras*/
+    struct dalumno
+    {
+        int matricula;
+        char nombre[50];
+        char direccion[50];
+        char carrera[50];
+        float promedio;
+    };
+
+    return 0;
+}
